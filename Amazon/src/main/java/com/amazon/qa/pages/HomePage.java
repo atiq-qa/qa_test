3ceb2deccad7a814c693846a4b82faa71a267b0f
Niazai <Niazai@10.0.0.161>
2021-04-25 10:51:39 -0700

    New build

--- Amazon/src/main/java/com/amazon/qa/pages/HomePage.java
+++ Amazon/src/main/java/com/amazon/qa/pages/HomePage.java
@@ -48,6 +48,8 @@ public class HomePage extends TestBase{
 
 	public void assetPrice()
 	{
+		List<WebElement> list = driver.findElements(By.xpath("//span[contains(text(),'Unit Testing Principles')]"));
+		list.get(0).click();
 		System.out.println("Asset Price before adding to cart: "+ assetPrice.getText());
 	}
 	
@@ -58,6 +60,8 @@ public class HomePage extends TestBase{
 	
 	public void clickOnProceedToCheckOut()
 	{
+		List<WebElement> list1 = driver.findElements(By.xpath("//span[@class='a-color-price hlb-price a-inline-block a-text-bold']"));
+		System.out.println("Asset price after adding to cart: "+list1.get(0).getText());
 		proceedToCheckOut.click();
 	}
 }
